457d48fa71378192294d0aee6f6ac2bd986eb767
uzairrrrrr <uzairawan156@gmail.com>
2025-02-03 16:05:27 +0500

    testing for deployement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,13 @@ import EssentialsSection from "@/components/essentials-sections";
 import DontMissSection from "@/components/dont-miss-section";
 import { fetchProducts } from "@/sanity/sanity.querry";
 
-// Fetch products server-side directly in the component
 const HomePage = async () => {
-  // Fetch the products using the function from sanity.query.ts
   const products = await fetchProducts();
 
   return (
     <main className="min-h-screen bg-white">
       <HeroSection />
       <ProductCarousel products={products} />{" "}
-      {/* Pass products to ProductCarousel */}
       <FeaturedSection />
       <GearUpSection />
       <DontMissSection />
